b3b8ca0b47fb62f7787d00ab007e81f680ee20bd
phamduc <phamdinhduchp2017@gmail.com>
2022-12-21 18:02:48 +0700

    UI

--- lib/screen/add_todo.dart
+++ lib/screen/add_todo.dart
@@ -1,1 +1,114 @@
+import 'package:flutter/material.dart';
 
+class AddToDo extends StatefulWidget {
+  const AddToDo({super.key});
+
+  @override
+  State<AddToDo> createState() => _AddToDoState();
+}
+
+class _AddToDoState extends State<AddToDo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xFF17171A),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close),
+        ),
+        title: null,
+        elevation: 0,
+        backgroundColor: const Color(0xFF141315),
+      ),
+      body: Column(
+        children: [
+          Container(
+            // height: MediaQuery.of(context).size.height * .1,
+            color: const Color(0xFF141315),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(children: const [
+                Text(
+                  'Add new',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )
+              ]),
+            ),
+          ),
+          const SizedBox(height: 50),
+          Container(
+            color: const Color(0xFF17171A),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(children: const [
+                TextField(
+                  decoration: InputDecoration(
+                    enabled: true,
+                    hintText: 'Task',
+                    hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF232328)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    enabled: true,
+                    hintText: 'Date',
+                    hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF232328)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    enabled: true,
+                    hintText: 'Emoji',
+                    hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF232328)),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF752ECF),
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(32)), // foreground
+                    ),
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child:
+                          Text('Create tasks', style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screen/view_todo.dart
+++ lib/screen/view_todo.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:todos_app/screen/add_todo.dart';
 
 class ViewTodo extends StatefulWidget {
   const ViewTodo({super.key});
@@ -9,8 +9,126 @@ class ViewTodo extends StatefulWidget {
 }
 
 class _ViewTodoState extends State<ViewTodo> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 60),
+                  const Text(
+                    'Daily Tasks',
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    height: 70,
+                    width: MediaQuery.of(context).size.width * .9,
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF17E0BC),
+                        borderRadius: BorderRadius.circular(16.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text(
+                            'Completed',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'To do Tasks',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  const SizedBox(height: 10),
+                  Card(
+                    color: const Color(0XFF232328),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 20),
+                      child: Row(
+                        children: [
+                          Transform.scale(
+                            scale: 1.8,
+                            child: Checkbox(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                                value: isChecked,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    isChecked = value!;
+                                  });
+                                }),
+                          ),
+                          const SizedBox(width: 10),
+                          Row(
+                            children: [
+                              const Text('🎉'),
+                              const SizedBox(width: 15),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Task 1 for day',
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.white),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    '21/12/2020',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Color(0xFF575762)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 80),
+                            child: Icon(
+                              Icons.delete_outline_rounded,
+                              size: 32,
+                              color: Colors.red,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddToDo()));
+        },
+        backgroundColor: const Color(0xFF752ECF),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
