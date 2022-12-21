b7dfcb63c47a8f868016d9d673f0fed9d406a9f8
phamduc <phamdinhduchp2017@gmail.com>
2022-12-21 09:49:27 +0700

    first commit

---
+++ lib/screen/view_todo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ViewTodo extends StatefulWidget {
+  const ViewTodo({super.key});
+
+  @override
+  State<ViewTodo> createState() => _ViewTodoState();
+}
+
+class _ViewTodoState extends State<ViewTodo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
